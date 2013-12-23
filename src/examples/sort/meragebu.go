fc00560bba631611a0fce7ef5f624578b6eb88b2
March Liu <march.liu@gmail.com>
2013-12-23 17:53:15 +0800

    add profile into merge sort buttom up

--- src/examples/sort/meragebu.go
+++ src/examples/sort/meragebu.go
@@ -6,14 +6,34 @@ import (
 	atypes "algorithms/types"
 	"math/rand"
 	"runtime"
+	"runtime/pprof"
+	"flag"
+	"log"
+	"os"
 )
 
 func main(){
-	var length = 10
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	flag.Parse()
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	if *cpuprofile==""{
+		run()
+	} else {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+		run()
+	}
+}
+
+func run(){
+	var length = 10000
 	var c = make(atypes.Sortable, length)
 	for i:=0; i<length; i++ {
-		var data = atypes.Integer(rand.Intn(1000))
+		var data = atypes.Integer(rand.Intn(1000000000))
 		c[i] = data
 	}
 	fmt.Printf("before:%v\n", c)
